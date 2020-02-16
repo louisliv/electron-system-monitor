4b37f6efd371391bb262a8d5f56dc111da869f12
Louis Livingston <livingston.louis@gmail.com>
2020-02-15 22:14:59 -0500

    Dev environment stable.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # electron-system-monitor
 System Monitor based on Electron
+
+## Dependancies
+
+* Node.js
+
+## Dev Setup
+1. Clone the repo.
+2. `cd electron-system-monitor/system-monitor`
+3. `npm install`
+4. `npm run dev`
+5. Any changes made to the files in `src` should 
+automatically refresh the app when the file is saved
---
+++ system-monitor/src/main.js
@@ -0,0 +1,62 @@
+'use strict'
+
+const electron = require('electron')
+const app = electron.app
+const globalShortcut = electron.globalShortcut
+const os = require('os')
+const path = require('path')
+const config = require('../package.json')
+const BrowserWindow = electron.BrowserWindow
+
+app.setName(config.productName)
+var mainWindow = null
+app.on('ready', function () {
+  mainWindow = new BrowserWindow({
+    backgroundColor: 'lightgray',
+    title: config.productName,
+    show: false,
+    webPreferences: {
+      nodeIntegration: true,
+      defaultEncoding: 'UTF-8'
+    },
+    fullscreen: true,
+    frame: false,
+    icon: __dirname + 'monitor-icon.icns'
+  })
+
+  const startUrl = process.env.ELECTRON_START_URL || url.format({
+    pathname: path.join(__dirname, '/../build/index.html'),
+    protocol: 'file:',
+    slashes: true
+  });
+
+  mainWindow.loadURL(startUrl)
+
+  // Enable keyboard shortcuts for Developer Tools on various platforms.
+  let platform = os.platform()
+  if (platform === 'darwin') {
+    globalShortcut.register('Command+Option+I', () => {
+      mainWindow.webContents.openDevTools()
+    })
+  } else if (platform === 'linux' || platform === 'win32') {
+    globalShortcut.register('Control+Shift+I', () => {
+      mainWindow.webContents.openDevTools()
+    })
+  }
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.setMenu(null)
+    mainWindow.show()
+  })
+
+  mainWindow.onbeforeunload = (e) => {
+    // Prevent Command-R from unloading the window contents.
+    e.returnValue = false
+  }
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+})
+
+app.on('window-all-closed', () => { app.quit() })
